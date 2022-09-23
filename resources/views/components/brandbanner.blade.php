486f8b2781658abc7a0018077f3e2c95551a0bae
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-09-23 12:35:22 +0530

    Update brandbanner.blade.php

--- resources/views/components/brandbanner.blade.php
+++ resources/views/components/brandbanner.blade.php
@@ -46,7 +46,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
     integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://use.fontawesome.com/abfdd2ac1e.js"></script>
+/*<script src="https://use.fontawesome.com/abfdd2ac1e.js"></script>*/
 
 
 
